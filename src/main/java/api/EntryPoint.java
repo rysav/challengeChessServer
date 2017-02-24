cdca590ffb82828fa1cb7f2ad27a724e096379ab
hunterrees <hunter.rees13@gmail.com>
2017-02-23 21:49:11 -0700

    Added Spring Boot dependencies

--- src/main/java/api/EntryPoint.java
+++ src/main/java/api/EntryPoint.java
@@ -2,8 +2,18 @@ package api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 class EntryPoint {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(EntryPoint.class);
+
+  public static void main(String[] args) throws Exception {
+    Object[] sources = {EntryPoint.class};
+    SpringApplication.run(sources, args);
+  }
 }
